d3c2a2e9aa0d65172d3a87fd4f0bd71e2da75f6c
morochena <marcus.orochena@gmail.com>
2018-06-16 12:57:09 -0400

    removed unnecessary files

--- src/screens/index.js
+++
@@ -1,7 +0,0 @@
-import { Navigation } from "react-native-navigation";
-
-import TodoList from "../components/TodoList";
-
-export function registerScreens() {
-  Navigation.registerComponent("TodoList", () => TodoList);
-}
